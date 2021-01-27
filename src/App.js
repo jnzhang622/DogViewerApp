4c4b3539cb3d66e26f3d5bc7808d516f1d340336
jnzhang622 <jnzhang622@gmail.com>
2021-01-26 19:29:31 -0500

    rendering all pics of certain dogs capable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state={
+    allBreeds: [],
+    selectedBreedImgs: [],
+  }
 
+  componentDidMount(){
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(resp => resp.json())
+    .then(data => this.setState({allBreeds: data.message}))
+  }
+
+  selectedBreed = (e) => {
+    this.setState({selectedBreed: e.target.value})
+    fetch(`https://dog.ceo/api/breed/${e.target.value}/images`)
+    .then(resp => resp.json())
+    .then(data => this.setState({selectedBreedImgs: data.message}))
+  }
+
+  render(){
+    // console.log(Object.values(this.state.allBreeds))
+    console.log(this.state.selectedBreedImgs)
+    return (
+      <div className="App">
+        <div>
+          <select className="" onChange={this.selectedBreed}>
+            <option></option>
+            {Object.keys(this.state.allBreeds).map(breed => {
+              return(
+                <option value={breed}>{breed}</option>
+              )
+            })}
+          </select>
+        </div>
+        <div>
+          {this.state.selectedBreedImgs.map(dogImg => {
+            return (
+              <img src={dogImg} width="200px"/>
+            )
+          }
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
